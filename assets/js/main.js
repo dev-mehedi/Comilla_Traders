815218a5be633ed050328fdb5f1f475055e49669
Md Mahmudur Rahman <dev-mehedi@users.noreply.github.com>
2024-05-28 00:49:50 +0300

    Comilla_Traders

---
+++ assets/js/main.js
@@ -0,0 +1,105 @@
+$('#bootstrap-touch-slider').bsTouchSlider();
+
+// external js: ISOTOPE CUSTOMS JS ============
+
+// TOOLTIP
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows',
+  getSortData: {
+    name: '.name',
+    symbol: '.symbol',
+    number: '.number parseInt',
+    category: '[data-category]',
+    weight: function( itemElem ) {
+      var weight = $( itemElem ).find('.weight').text();
+      return parseFloat( weight.replace( /[\(\)]/g, '') );
+    }
+  }
+});
+
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
+
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+
+// bind sort button click
+$('#sorts').on( 'click', 'button', function() {
+  var sortByValue = $(this).attr('data-sort-by');
+  $grid.isotope({ sortBy: sortByValue });
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
+
+
+// COUNTERUP
+
+$('.counter').counterUp({
+  delay: 10,
+  time: 2000
+});
+
+
+// OWL 
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:false,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+
+
+$("a[href='#top']").click(function() {
+     $("html, body").animate({ scrollTop: 0 }, "slow");
+     return false;
+  });
+
+//  WOW JS
+wow = new WOW(
+  {
+    animateClass: 'animated',
+    offset:       100,
+    callback:     function(box) {
+      console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+  }
+);
+wow.init();
+document.getElementById('moar').onclick = function() {
+  var section = document.createElement('section');
+  section.className = 'section--purple wow fadeInDown';
+  this.parentNode.insertBefore(section, this);
+};
+
